0dd542b44586caccf50d2767810ff1ba0c94fe03
unknown <you@example.com>
2025-10-16 14:15:01 +0800

    update po

--- proj.cpp
+++ proj.cpp
@@ -1,18 +1,76 @@
  #include <iostream>
  #include <iomanip>
+ #include <vector>
 using namespace std;
 
  	class Batteries{
 		private:
 			string brand; 
+			
 		
-		public: 
+			
+			public: 
 			void setBrand(string b){
 				brand = b;
 			}
 			string getBrand (string b){
-            	return b;
+				return b;
         	}
+			
+			struct batt {
+				int id;
+				string name;
+				int quantity;
+				int price;
+			};
+
+		
+			vector<batt> renata_arr = {
+				{1, "Renata 314", 10, 500},
+				{2, "Renata 510", 10, 500},
+				{3, "Renata 600", 10, 500},
+				{4, "Renata 700", 10, 500},
+			};
+
+			vector<batt> maxwell_arr = {
+				{1, "Maxwell 314", 10, 500},
+				{2, "Maxwell 510", 10, 500},
+				{3, "Maxwell 600", 10, 500},
+				{4, "Maxwell 700", 10, 500},
+			};
+			// int size = sizeof(hihi) / sizeof(hihi[0]);
+
+			// int battery_list(int i){
+			// 	for(int i = 0; i < size; i++){
+			// 		cout << "ID: " << hihi[i].id << endl;
+			// 	}
+			// }
+			// int switch_bat (int bat){
+			// 	 switch(bat){
+			// 		case 1:
+			// 			cout << "\nMAXWELL\n";
+			// 			// details
+			// 			break;
+			// 		case 2:
+			// 			cout << "\nRENATA\n"; 
+				
+						//array function
+						//call function 
+						// int index = 0; 
+						// for(int i = 0; i < 5; i++){
+						// 	cout << setw(20) << left << items[index++];
+						// 	cout << setw(20) << left << items[index++];
+						// 	//make function
+						// 	//return variabke 
+						// 	//call here
+						// 	cout << endl;
+						// }        
+						
+						// int hihi = battery_list(bat);
+						// return hihi;
+						// break;                      
+			// 	}
+			// };
 	};
 
 	class Straps{
@@ -57,72 +115,34 @@ using namespace std;
             	return price;
         	}
 
-			int switch_bat (int bat){
-				 switch(bat){
-					case 1:
-						cout << "\nMAXWELL\n";
-						// details
-						break;
-					case 2:
-						cout << "\nRENATA\n"; 
-						string items[16] = {
-							"Renata 315", "Renata 315",
-							"Renata 315", "Renata 315",
-							"Renata 315", "Renata 315",
-							"Renata 315", "Renata 315",
-							"Renata 315", "Renata 315",
-							"Renata 315", "Renata 315",
-							"Renata 315", "Renata 315",
-							"Renata 315", "Renata 315",
-						};
-						//array function
-						//call function 
-						int index = 0; 
-						for(int i = 0; i < 5; i++){
-							cout << setw(20) << left << items[index++];
-							cout << setw(20) << left << items[index++];
-							//make function
-							//return variabke 
-							//call here
-							cout << endl;
-						}                                    
-						break;                      
-				}
-			}
-			int switch_bat (int bat){
-				 switch(bat){
-					case 1:
-						cout << "\nMAXWELL\n";
-						// details
-						break;
-					case 2:
-						cout << "\nRENATA\n"; 
-						string items[10] = {
-							"Renata 315", "Renata 315",
-							"Renata 315", "Renata 315",
-							"Renata 315", "Renata 315",
-							"Renata 315", "Renata 315",
-							"Renata 315", "Renata 315",
-						};
-						//array function
-						//call function 
-						int index = 0; 
-						for(int i = 0; i < 5; i++){
-							cout << setw(20) << left << items[index++];
-							cout << setw(20) << left << items[index++];
-							//make function
-							//return variabke 
-							//call here
-							cout << endl;
-						}                                    
-						break;                      
-				}
-			}
+			// int switch_bat (int bat){
+			// 	 switch(bat){
+			// 		case 1:
+			// 			cout << "\nMAXWELL\n";
+			// 			// details
+			// 			break;
+			// 		case 2:
+			// 			cout << "\nRENATA\n"; 
+						
+			// 			//array function
+			// 			//call function 
+			// 			int index = 0; 
+			// 			for(int i = 0; i < 5; i++){
+			// 				cout << setw(20) << left << items[index++];
+			// 				cout << setw(20) << left << items[index++];
+			// 				//make function
+			// 				//return variabke 
+			// 				//call here
+			// 				cout << endl;
+			// 			}                                    
+			// 			break;                      
+			// 	}
+			// } 
 	} ;
 
 int main(){ 
 	int opt, mat, inv, pos, bat, strp;
-	Straps obj; 
+	Batteries battery; 
     
 	do{
     	cout << "\n\nPICK FROM THE FOLLOWING: \n";
@@ -246,7 +266,7 @@ int main(){
 											cin >> bat;
 
 				
-											obj.switch_bat(bat); 
+											// battery.switch_bat(bat); 
 
 										}while(bat != 0);
 									    break;
