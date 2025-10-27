f15907a62a59bea97f180134108827d559005ad9
Clare Lubiano <qshsulla@tip.edu.ph>
2025-10-27 17:53:18 +0800

    okay na toh

--- batteries.h
+++ batteries.h
@@ -6,186 +6,315 @@
 #include <algorithm>
 #include <cctype>
 
- using namespace std;
+using namespace std;
 
-class Batteries{
-		private:
-			string brand; 
-			
-		public: 
-			void setBrand(string b){
-				brand = b;
-			}
-			string getBrand (string b){
-				return b;
-        	}
-			
-			struct BattItems {
-				int id;
-				string categ;
-				string brand;
-				int quantity;
-				int price;
+class Batteries
+{
+private:
+	string brand;
 
-				BattItems(int i, string ct, string b, int q, double p) //this is called a constructor, it reads(?) the data from the array in order.
-				: id(i), categ(ct), brand(b), quantity(q), price(p) {}
-			};
-			
-			vector<BattItems> allbatt_arr = {
-				{1, "renata", "Renata 314", 10, 500},
-				{2, "renata", "Renata 510", 10, 500},
-				{3, "renata", "Renata 600", 10, 500},
-				{4, "renata", "Renata 700", 10, 500},
+public:
+	void setBrand(string b)
+	{
+		brand = b;
+	}
+	string getBrand(string b)
+	{
+		return b;
+	}
 
-				{5, "maxwell", "Maxwell 314", 10, 500},
-				{6, "maxwell", "Maxwell 510", 10, 500},
-				{7, "maxwell", "Maxwell 600", 10, 500},
-				{8, "maxwell", "Maxwell 700", 10, 500}
-			};
+	struct BattItems
+	{
+		int id;
+		string categ;
+		string brand;
+		int quantity;
+		int price;
 
-		int DisplayRenata(vector<BattItems> arr, string categ) {
-			cout << "\n" << categ << " \n";
-			cout << left << setw(5) << "ID"
-				<< setw(15) << "Brand"
-				<< setw(10) << "Qty"
-				<< setw(10) << "Price" << "\n";
-
-			for (auto &i : arr) {
-				if(i.categ == "renata"){
-					cout << left << setw(5) << i.id
-						<< setw(15) << i.brand
-						<< setw(10) << i.quantity
-						<< setw(10) << i.price << "\n";
-				}
-			}
-			return 0;
+		BattItems(int i, string ct, string b, int q, double p) // this is called a constructor, it reads(?) the data from the array in order.
+			: id(i), categ(ct), brand(b), quantity(q), price(p)
+		{
 		}
-			
-		int DisplayMaxwell(vector<BattItems> arr, string categ) {
-			cout << "\n" << categ << " \n";
-			cout << left << setw(5) << "ID"
-				<< setw(15) << "Brand"
-				<< setw(10) << "Qty"
-				<< setw(10) << "Price" << "\n";
+	};
 
-			for (auto &i : arr) {
-				if(i.categ == "maxwell"){
-					cout << left << setw(5) << i.id
-						<< setw(15) << i.brand
-						<< setw(10) << i.quantity
-						<< setw(10) << i.price << "\n";
-				}
+	vector<BattItems> allbatt_arr = {
+		{1, "renata", "Renata 314", 10, 500},
+		{2, "renata", "Renata 510", 10, 500},
+		{3, "renata", "Renata 600", 10, 500},
+		{4, "renata", "Renata 700", 10, 500},
+
+		{5, "maxwell", "Maxwell 314", 10, 500},
+		{6, "maxwell", "Maxwell 510", 10, 500},
+		{7, "maxwell", "Maxwell 600", 10, 500},
+		{8, "maxwell", "Maxwell 700", 10, 500}};
+
+	int DisplayRenata(vector<BattItems> arr, string categ)
+	{
+		cout << "\n"
+			 << categ << " \n";
+		cout << left << setw(5) << "ID"
+			 << setw(15) << "Brand"
+			 << setw(10) << "Qty"
+			 << setw(10) << "Price" << "\n";
+
+		for (auto &i : arr)
+		{
+			if (i.categ == "renata")
+			{
+				cout << left << setw(5) << i.id
+					 << setw(15) << i.brand
+					 << setw(10) << i.quantity
+					 << setw(10) << i.price << "\n";
 			}
-			return 0;
+		}
+		return 0;
+	}
+
+	int DisplayMaxwell(vector<BattItems> arr, string categ)
+	{
+		cout << "\n"
+			 << categ << " \n";
+		cout << left << setw(5) << "ID"
+			 << setw(15) << "Brand"
+			 << setw(10) << "Qty"
+			 << setw(10) << "Price" << "\n";
+
+		for (auto &i : arr)
+		{
+			if (i.categ == "maxwell")
+			{
+				cout << left << setw(5) << i.id
+					 << setw(15) << i.brand
+					 << setw(10) << i.quantity
+					 << setw(10) << i.price << "\n";
+			}
+		}
+		return 0;
+	}
+
+	void switch_view(int bat)
+	{
+		switch (bat)
+		{
+		case 1:
+			DisplayMaxwell(allbatt_arr, "MAXWELL BATTERY");
+			break;
+
+		case 2:
+			DisplayRenata(allbatt_arr, "RENATA BATTERY");
+			break;
+
+		case 3:
+			DisplayMaxwell(allbatt_arr, "MAXWELL BATTERY");
+			DisplayRenata(allbatt_arr, "RENATA BATTERY");
+			break;
+		}
+	}
+};
+
+class BatteryCRUD
+{
+private:
+	Batteries &batt;
+	vector<Batteries::BattItems> &allbatt_arr;
+	// static int nextID;
+
+public:
+	BatteryCRUD(Batteries &batts) : batt(batts), allbatt_arr(batts.allbatt_arr) {}
+
+	// DISPLAY
+	void displayItems()
+	{
+		batt.DisplayMaxwell(allbatt_arr, "MAXWELL BATTERY");
+		batt.DisplayRenata(allbatt_arr, "RENATA BATTERY");
+	}
+	// EDIT
+	void editItems(int id, string newCateg, string newBrand, int newQty, int newPrice)
+	{
+
+		transform(newCateg.begin(), newCateg.end(), newCateg.begin(),
+				  [](unsigned char c)
+				  {
+					  return tolower(c);
+				  });
+
+		cout << newBrand;
+
+		transform(newBrand.begin(), newBrand.end(), newBrand.begin(),
+				  [](unsigned char br)
+				  {
+					  return tolower(br);
+				  });
+
+		if (!newBrand.empty())
+		{
+			newBrand[0] = toupper(newBrand[0]);
 		}
 
-		void switch_view(int bat){
-			switch(bat){
-				case 1:
-					DisplayMaxwell(allbatt_arr, "MAXWELL BATTERY");
-					break;
-
-				case 2:
-					DisplayRenata(allbatt_arr, "RENATA BATTERY");           
-					break;    
-
-				case 3:
-					DisplayMaxwell(allbatt_arr, "MAXWELL BATTERY");
-					DisplayRenata(allbatt_arr, "RENATA BATTERY"); 
-					break;   
-
-				}	
-			}	
-		};
-
-		class BatteryCRUD {
-			private:
-				Batteries& batt;
-				// int geh;
-				vector<Batteries::BattItems>& allbatt_arr;
-				// vector<Batteries::BattItems>& heuy;
-					
-			public:
-
-				BatteryCRUD(Batteries& batts) : batt(batts), allbatt_arr(batts.allbatt_arr){}
-
-				//EDIT
-			void editItems(string categ, int id, string newCateg, int newQty, int newPrice ) {
-				
-				transform(categ.begin(), categ.end(), categ.begin(), 
-					[](unsigned char c)
-					{
-						return tolower(c);
-					}
-				);
-
-				if(!categ.empty()){
-					categ[0] = toupper(categ[0]);
-				}
-
-				batt.allbatt_arr = allbatt_arr;
-				//play with this
-				for(auto &b : allbatt_arr) {
-					if(b.id == id) {
-						b.categ = newCateg;
-						b.quantity = newQty;
-						b.price = newPrice;
-						cout << "Item updated successfully!\n";
-						return;
-					}
-				}
-
-				cout << "Battery with ID " << id << " not found.\n";
+		batt.allbatt_arr = allbatt_arr;
+		cout << newCateg << " " << newBrand;
+		// play with this
+		for (auto &b : allbatt_arr)
+		{
+			if (b.id == id)
+			{
+				b.categ = newCateg;
+				b.brand = newBrand;
+				b.quantity = newQty;
+				b.price = newPrice;
+				cout << "Item updated successfully!\n";
+				return;
 			}
+		}
 
-			//ADD
-			void addItems(string categ, int id, string brand, int quantity, int price) { 
-				vector<Batteries::BattItems>& allbatt_arr;
-				
-				Batteries::BattItems newBatt(id, categ, brand, quantity, price);
+		cout << "Battery with ID " << id << " not found.\n";
+	}
 
-				transform(categ.begin(), categ.end(), categ.begin(), 
-					[](unsigned char c){return tolower(c);}
-				);
+	// ADD
+	void addItems(string categ, int id, string itemName, int quantity, int price)
+	{
+		batt.allbatt_arr;
+		// id = nextID++;
 
-				if(categ == "renata" || categ == "maxwell"){
-					batt.allbatt_arr.push_back(newBatt);
+		Batteries::BattItems newBatt(id, categ, itemName, quantity, price);
 
-					cout << newBatt.brand << " " << newBatt.id;
-					batt.DisplayMaxwell(allbatt_arr, "MAXWELL BATTERY");
-					batt.DisplayRenata(allbatt_arr, "RENATA BATTERY");
-					return;
-				}	
-				else{
-					cout << "Brand Name not found :(\n";
-					return;
-				}
+		transform(categ.begin(), categ.end(), categ.begin(),
+				  [](unsigned char c)
+				  { return tolower(c); });
 
-				cout << "Battery successfully added to " << categ << " list.\n";
+		if (categ == "renata" || categ == "maxwell")
+		{
+			batt.allbatt_arr.push_back(newBatt);
+
+			cout << newBatt.brand << " " << newBatt.id;
+			batt.DisplayMaxwell(allbatt_arr, "MAXWELL BATTERY");
+			batt.DisplayRenata(allbatt_arr, "RENATA BATTERY");
+			cout << "Battery successfully added to " << categ << " list.\n";
+			return;
+		}
+		else
+		{
+			cout << "Brand Name not found :(\n";
+			return;
+		}
+	}
+
+	// DELETE
+	void deleteItems(int id, int dec)
+	{
+		if (dec == 1)
+		{
+			batt.DisplayMaxwell(allbatt_arr, "MAXWELL BATTERY");
+			batt.DisplayRenata(allbatt_arr, "RENATA BATTERY");
+			allbatt_arr.erase(allbatt_arr.begin());
+			cout << "Item deleted successfully.\n";
+			cout << id;
+			batt.DisplayMaxwell(allbatt_arr, "MAXWELL BATTERY");
+			batt.DisplayRenata(allbatt_arr, "RENATA BATTERY");
+			return;
+		}
+		else
+		{
+			cout << "Item not deleted.";
+			return;
+		}
+		cout << "Item with ID " << id << " not found.\n";
+	}
+
+	// LEVEINSHTEIN DISTANCE (para sa search)
+	int levenshteinDistance(const string &s1, const string &s2)
+	{
+		const size_t len1 = s1.size(), len2 = s2.size();
+		vector<vector<int>> dp(len1 + 1, vector<int>(len2 + 1));
+
+		for (size_t i = 0; i <= len1; ++i)
+			dp[i][0] = i;
+		for (size_t j = 0; j <= len2; ++j)
+			dp[0][j] = j;
+
+		for (size_t i = 1; i <= len1; ++i)
+			for (size_t j = 1; j <= len2; ++j)
+				dp[i][j] = min({
+					dp[i - 1][j] + 1,							// deletion
+					dp[i][j - 1] + 1,							// insertion
+					dp[i - 1][j - 1] + (s1[i - 1] != s2[j - 1]) // substitution
+				});
+
+		return dp[len1][len2];
+	}
+
+	// forda correct brand name
+	string correctItemName(string input)
+	{
+		vector<string> validBrands = {"Renata", "renata", "maxwell", "Maxwell"};
+
+		// transform the input to lowercase
+		transform(input.begin(), input.end(), input.begin(),
+				  [](unsigned char i)
+				  {
+					  return tolower(i);
+				  });
+
+		// trnasform the first char to uppercase
+		if (!input.empty())
+		{
+			input[0] = toupper(input[0]);
+		}
+
+		return input;
+
+		// store varibles
+		string bestMatch = input;
+		int bestScore = INT_MAX;
+
+		// ich-check sa validbrand kung ano ang best match
+		for (const string &brand : validBrands)
+		{
+			int dist = levenshteinDistance(input, brand);
+			if (dist < bestScore)
+			{
+				bestScore = dist;
+				bestMatch = brand;
 			}
+		}
+		if (bestScore <= 2) // only accepts 2 typos
+			return bestMatch;
+		else
+			return input; // too different, keep original
+	}
 
-			//DELETE
-			void deleteItems(int id, int dec) {
-				// batt.allbatt_arr
-				// allbatt_arr* hi = &heuy;
-				// batt.BattItems(int i) 
-				// : id(i){}
-				if(dec == 1 ){
-					// for(auto it = allbatt_arr.begin(); it != allbatt_arr.end(); it++) {
-						// if(it->id == id) {
-							allbatt_arr.erase(allbatt_arr.begin() );
-							cout << "Item deleted successfully.\n";
-							cout << id;
-							batt.DisplayMaxwell(allbatt_arr, "MAXWELL BATTERY");
-							batt.DisplayRenata(allbatt_arr, "RENATA BATTERY"); 
-							return;
-						// }
-					// }
-				}	
-				else{
-					cout << "Item not deleted.";
-					return;
-				}
-				cout << "Item with ID " << id << " not found.\n";
+	// SEARCH
+	void searchItems(string input)
+	{
+		string brandPart, number;
+
+		stringstream ss(input);
+		ss >> brandPart;
+		getline(ss, number);							// separates the brandPart from the number part
+		number.erase(0, number.find_first_not_of(" ")); // erases the excess space from the number part
+
+		// search the brand part
+		string correctBrand = correctItemName(brandPart);
+
+		bool found = false;
+		for (const auto &batt : allbatt_arr)
+		{
+			string battNameLower = batt.brand; // brand includes the full name, e.g., "Renata 567"
+			string inputLower = correctBrand;
+
+			transform(battNameLower.begin(), battNameLower.end(), battNameLower.begin(), ::tolower);
+			transform(inputLower.begin(), inputLower.end(), inputLower.begin(), ::tolower);
+
+			if (battNameLower.find(inputLower) != string::npos) // partial match
+			{
+				cout << batt.brand << endl;
+				found = true;
 			}
-		};
+		}
+
+		if (!found)
+		{
+			cout << "No items matches :<\n";
+		}
+	}
+};
--- proj.cpp
+++ proj.cpp
@@ -1,216 +1,234 @@
- #include <iostream>
- #include <vector>
- #include <string>
- #include "batteries.h"
- #include "straps.h"
- #include "pos.h"
- using namespace std;
+#include <iostream>
+#include <vector>
+#include <string>
+#include "batteries.h"
+#include "straps.h"
+#include "pos.h"
+using namespace std;
 
-int main(){ 
-	int opt, mat, inv, pos, sale=0, edit, search, bat, strp;
+int main()
+{
+	int opt, mat, inv, pos, sale = 0, edit, search, bat, strp;
 	Batteries b;
-	string brandName, newBrand;
+	string brandName, newItem, input;
 	int newQty, newPrice, id, dec;
 	BatteryCRUD crud(b);
-	Straps s; 
-	Pos myPOS(s); 
-    
-	do{
-    	cout << "\n\nPICK FROM THE FOLLOWING: \n";
-    	cout << "[1] INVENTORY\n";
-    	cout << "[2] POINT OF SALES (POS)\n";
-    	cout << "[3] ADD RECEIPT\n";
-    	cout << "[0] End\n\n";
-    	cout << "Choose an option: ";
-    	cin >> opt;
-   	 
-    
-        	switch (opt){
-            	case 1: 
-					cout << "\nINVENTORY\n";
-					cout << "[1] EDIT\n";
-					cout << "[2] VIEW\n";
+	Straps s;
+	Pos myPOS(s);
+
+	do
+	{
+		cout << "\n\nPICK FROM THE FOLLOWING: \n";
+		cout << "[1] INVENTORY\n";
+		cout << "[2] POINT OF SALES (POS)\n";
+		cout << "[3] ADD RECEIPT\n";
+		cout << "[0] End\n\n";
+		cout << "Choose an option: ";
+		cin >> opt;
+
+		switch (opt)
+		{
+		case 1:
+			cout << "\nINVENTORY\n";
+			cout << "[1] EDIT\n";
+			cout << "[2] VIEW\n";
+			cout << "[0] back\n\n";
+			cout << "Choose an option: ";
+			cin >> inv;
+
+			do
+			{
+				switch (inv)
+				{
+				case 1:
+					cout << "\nEDIT MATERIALS: \n";
+					cout << "[1] STRAPS\n";
+					cout << "[2] BATTERIES\n";
 					cout << "[0] back\n\n";
 					cout << "Choose an option: ";
-					cin >> inv;
-					
-					do{
-						switch (inv){
+					cin >> edit;
+
+					switch (edit)
+					{
+					case 1:
+						do
+						{
+							cout << "\nSTRAPS\n";
+							cout << "[1] ADD ITEM\n";
+							cout << "[2] EDIT ITEM\n";
+							cout << "[3] DELETE ITEM\n";
+							cout << "[4] DISPLAY ALL ITEMS\n"; // i think this is not needed(?) since it will fall under view option
+							cout << "[5] SEARCH NAME OF STRAP \n";
+							cout << "[0] back\n";
+							cout << "Choose an option: ";
+							cin >> strp;
+
+							s.switch_edit(strp);
+						} while (strp != 0);
+						break;
+
+					case 2:
+						do
+						{
+							cout << "\nBATTERIES\n";
+							cout << "[1] EDIT\n";
+							cout << "[2] ADD\n";
+							cout << "[3] DELETE\n";
+							cout << "[4] SEARCH NAME OF BATTERY \n";
+							cout << "[0] back\n";
+							cout << "Choose an option: ";
+							cin >> bat;
+
+							switch (bat)
+							{
 							case 1:
-								cout << "\nEDIT MATERIALS: \n";
-								cout << "[1] STRAPS\n";
-								cout << "[2] BATTERIES\n";
-								cout << "[0] back\n\n";
-								cout << "Choose an option: ";
-								cin >> edit;
-							
-								switch (edit){
-									case 1: 
-                                    do{
-										cout << "\nSTRAPS\n";
-                                        cout << "[1] ADD ITEM\n";
-                                        cout << "[2] EDIT ITEM\n";
-                                        cout << "[3] DELETE ITEM\n";                                    
-                                        cout << "[4] DISPLAY ALL ITEMS\n";  //i think this is not needed(?) since it will fall under view option                                  
-                                        cout << "[5] SEARCH NAME OF STRAP \n";
-                                        cout << "[0] back\n";
-                                        cout << "Choose an option: ";
-                                        cin >> strp;
+								crud.displayItems();
 
-                                        s.switch_edit(strp);
-                                    }while(strp !=0);
-										break;
+								cout << "Input Item ID you want to edit: ";
+								cin >> id;
+								cout << "Input new brand name [Renata/Maxwell]: ";
+								cin >> brandName;
+								cin.ignore(numeric_limits<streamsize>::max(), '\n');
+								cout << "Input new item name: ";
+								getline(cin, newItem);
+								cout << "Input new quantity: ";
+								cin >> newQty;
+								cout << "Input new price: ";
+								cin >> newPrice;
 
-									case 2: 
-                                    do{
-										cout << "\nBATTERIES\n";
-                                        cout << "[1] EDIT\n";
-                                        cout << "[2] ADD\n";
-                                        cout << "[3] DELETE\n";                                    
-                                        cout << "[4] SEARCH NAME OF BATTERY \n";
-                                        cout << "[0] back\n";
-                                        cout << "Choose an option: ";
-                                        cin >> bat;
+								crud.editItems(id, brandName, newItem, newQty, newPrice);
 
-                                        switch(bat){
-                                            case 1:
-												cout << "\nInput brand name you want to edit [Renata/Maxwell]: ";
-												cin >> brandName;
-												cout << "Input Item ID: ";
-												cin >> id;
-												cout << "Input new brand: ";
-												cin >> newBrand;
-												cout << "Input new quantity: ";
-												cin.ignore(numeric_limits<streamsize>::max(), '\n');
-												cin >> newQty;
-												cout << "Input new price: ";
-												cin >> newPrice;
-
-												crud.editItems(brandName, id, newBrand, newQty, newPrice);
-
-                                            	break;
-                                            case 2:
-												cout << "\nInput brand name you want to add [Renata/Maxwell]: ";
-												cin >> brandName;
-												cout << "Input Item ID: ";
-												cin >> id;
-												cout << "Input new brand: ";
-												cin >> newBrand;
-												cout << "Input new quantity: ";
-												cin.ignore(numeric_limits<streamsize>::max(), '\n');
-												cin >> newQty;
-												cout << "Input new price: ";
-												cin >> newPrice;
-
-												crud.addItems(brandName, id, newBrand, newQty, newPrice);
-
-                                            	break;
-                                            case 3:
-												cout << "Input ID you want to delete: ";
-												cin >> id;
-												cout << "Are you sure you want to delete item? [1-Yes/0-No]: ";
-												cin >> dec;
-
-												crud.deleteItems(id, dec);
-												
-                                            	break;
-                                            case 4:
-												cout << "SEARCH";
-                                            	break;
-                                        }
-                                    }while(bat != 0);   
-                                        break;
-								}
 								break;
-
 							case 2:
-								cout << "\nVIEW LIST OF MATERIALS: \n";
-								cout << "[1] STRAPS\n";
-								cout << "[2] BATTERIES\n";
-								cout << "[0] back\n\n";
-								cout << "Choose an option: ";
-								cin >> mat;
-								                                                 
-								switch (mat){
-									case 1: 
-										do{
-											cout << "\nSTRAPS\n";
-											cout << "[1] WITH STITCH STRAP\n";
-											cout << "[2] NO STITCH STRAP\n";
-											cout << "[3] CASIO STRAP\n";
-											cout << "[4] DISPLAY ALL STRAPS\n";
-											cout << "[5] SEARCH NAME OF STRAP \n";
-											cout << "[0] back\n";
-											cout << "Choose an option: ";
-											cin >> strp;
+								cout << "\nInput brand name you want to add [Renata/Maxwell]: ";
+								cin >> brandName;
+								cout << "Input Item ID: ";
+								cin >> id;
+								cout << "Input new item name: ";
+								cin >> newItem;
+								cout << "Input new quantity: ";
+								cin.ignore(numeric_limits<streamsize>::max(), '\n');
+								cin >> newQty;
+								cout << "Input new price: ";
+								cin >> newPrice;
 
-											s.switch_view(strp);
-											
-										}while(strp != 0);
-										break;
-									 
-									case 2: 
-										do{
-											cout << "\nBATTERIES\n";
-											cout << "[1] MAXWELL\n";
-											cout << "[2] RENATA\n";
-											cout << "[3] DISPLAY ALL BATTERIES\n";
-											cout << "[4] SEARCH NAME OF BATTERY \n";
-											cout << "[0] back\n";
-											cout << "Choose an option: ";
-											cin >> bat;
-											
-											b.switch_view(bat);
+								crud.addItems(brandName, id, newItem, newQty, newPrice);
 
-										}while(bat != 0);
-									    break;
-                                }
 								break;
-								do {
-									cout << "\nVIEW LIST OF MATERIALS: \n";
-									cout << "[1] STRAPS\n";
-									cout << "[2] BATTERIES\n";
-									cout << "[0] back\n\n";
-									cout << "Choose an option: ";
-									cin >> mat;
-								
-									switch (mat){
-										case 1: 
-											cout << " STRAPS\n";
-											break;
-										case 2: 
-											cout << "BATTERIES\n";
-											break;
-									}
-								}while (mat != 0);
-							break;
-						}
+							case 3:
+								cout << "Input ID you want to delete: ";
+								cin >> id;
+								cout << "Are you sure you want to delete item? [1-Yes/0-No]: ";
+								cin >> dec;
 
-					}while(inv != 0 && mat != 0 && edit != 0);
+								crud.deleteItems(id, dec);
 
-            	break;
-           	 
-            	case 2:
-					do{
-						cout << "\nPOINT OF SALES (POS)\n";
-						cout << "[1] OVERVIEW OF SALES\n";
-						cout << "[2] SEARCH DATE OF SALES\n";
-						cout << "[3] HISTORY OF RECEIPTS\n";
-						cout << "[0] back\n\n";
-						cout << "Choose an option: ";
-						cin >> pos;
-						
-						myPOS.POS_switch(pos, sale);
-					}while(pos != 0 && sale != 0);
-					break; 
+								break;
+							case 4:
+								cout << "SEARCH ITEM: ";
+								cin.ignore(numeric_limits<streamsize>::max(), '\n');
+								getline(cin, input);
 
-				case 3: 
-					myPOS.input();
+								crud.searchItems(input);
+								break;
+							}
+						} while (bat != 0);
+						break;
+					}
 					break;
 
-        	}
-	}while (opt != 0);    
+				case 2:
+					cout << "\nVIEW LIST OF MATERIALS: \n";
+					cout << "[1] STRAPS\n";
+					cout << "[2] BATTERIES\n";
+					cout << "[0] back\n\n";
+					cout << "Choose an option: ";
+					cin >> mat;
 
+					switch (mat)
+					{
+					case 1:
+						do
+						{
+							cout << "\nSTRAPS\n";
+							cout << "[1] WITH STITCH STRAP\n";
+							cout << "[2] NO STITCH STRAP\n";
+							cout << "[3] CASIO STRAP\n";
+							cout << "[4] DISPLAY ALL STRAPS\n";
+							cout << "[5] SEARCH NAME OF STRAP \n";
+							cout << "[0] back\n";
+							cout << "Choose an option: ";
+							cin >> strp;
+
+							s.switch_view(strp);
+
+						} while (strp != 0);
+						break;
+
+					case 2:
+						do
+						{
+							cout << "\nBATTERIES\n";
+							cout << "[1] MAXWELL\n";
+							cout << "[2] RENATA\n";
+							cout << "[3] DISPLAY ALL BATTERIES\n";
+							cout << "[4] SEARCH NAME OF BATTERY \n";
+							cout << "[0] back\n";
+							cout << "Choose an option: ";
+							cin >> bat;
+
+							b.switch_view(bat);
+
+						} while (bat != 0);
+						break;
+					}
+					break;
+					do
+					{
+						cout << "\nVIEW LIST OF MATERIALS: \n";
+						cout << "[1] STRAPS\n";
+						cout << "[2] BATTERIES\n";
+						cout << "[0] back\n\n";
+						cout << "Choose an option: ";
+						cin >> mat;
+
+						switch (mat)
+						{
+						case 1:
+							cout << " STRAPS\n";
+							break;
+						case 2:
+							cout << "BATTERIES\n";
+							break;
+						}
+					} while (mat != 0);
+					break;
+				}
+
+			} while (inv != 0 && mat != 0 && edit != 0);
+
+			break;
+
+		case 2:
+			do
+			{
+				cout << "\nPOINT OF SALES (POS)\n";
+				cout << "[1] OVERVIEW OF SALES\n";
+				cout << "[2] SEARCH DATE OF SALES\n";
+				cout << "[3] HISTORY OF RECEIPTS\n";
+				cout << "[0] back\n\n";
+				cout << "Choose an option: ";
+				cin >> pos;
+
+				myPOS.POS_switch(pos, sale);
+			} while (pos != 0 && sale != 0);
+			break;
+
+		case 3:
+			myPOS.input();
+			break;
+		}
+	} while (opt != 0);
 
 	return 0;
 }
