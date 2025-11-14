5dd76d09414997108476f7ef6b0da76c84ba9c5f
Shantee Naje <shanteenaje07@gmail.com>
2025-11-14 22:39:05 +0800

    price output

--- straps.h
+++ straps.h
@@ -47,87 +47,87 @@ public:
 	};
 
 	vector<StrapItems> allstraps_arr = {
-		{1, "stitched", "Black w/ black", "No type", "Buffalo Calf", 10, 7, "16-24", 950.00, ""},
-		{2, "stitched", "Black w/ white", "No type", "Buffalo Calf", 10, 7, "16-24", 950.00, ""},
-		{3, "stitched", "Brown dark", "No type", "Buffalo Calf", 8, 4, "16-24", 950.00, ""},
-		{4, "stitched", "Brown light", "No type", "Buffalo Calf", 8, 4, "16-24", 950.00, ""},
-		{5, "stitched", "Black w/ black", "No type", "Chrono Genuine", 10, 7, "20-24", 1200.00, ""},
-		{6, "stitched", "Black w/ white", "No type", "Chrono Genuine", 10, 7, "20-24", 1200.00, ""},
-		{7, "stitched", "Brown w/ white", "No type", "Chrono Genuine", 8, 4, "20-24", 1200.00, ""},
-		{8, "stitched", "Black", "No type", "Genuine", 10, 7, "16-24", 650.00, ""},
-		{9, "stitched", "Brown dark", "No type", "Genuine", 10, 7, "16-24", 650.00, ""},
-		{10, "stitched", "Brown light", "No type", "Genuine", 18, 4, "16-24", 650.00, ""},
-		{11, "stitched", "Gray", "No type", "Genuine", 8, 4, "16-24", 650.00, ""},
-		{12, "stitched", "Red", "No type", "Genuine", 8, 4, "16-24", 650.00, ""},
-		{13, "stitched", "Black w/ black", "No type", "Cobra Genuine", 10, 7, "16-24", 550.00, ""},
-		{14, "stitched", "Blue light w/ blue", "No type", "Cobra Genuine", 8, 4, "16-24", 550.00, ""},
-		{15, "stitched", "Brown dark w/ brown", "No type", "Cobra Genuine", 10, 7, "16-24", 550.00, ""},
-		{16, "stitched", "Brown light w/ brown", "No type", "Cobra Genuine", 8, 4, "16-24", 550.00, ""},
-		{17, "stitched", "Gray w/ blue", "No type", "Cobra Genuine", 8, 4, "16-24", 550.00, ""},
-		{18, "stitched", "Red w/ red", "No type", "Cobra Genuine", 8, 4, "16-24", 550.00, ""},
-		{19, "stitched", "Black w/ blue", "No type", "Silicone", 8, 4, "16-24", 750.00, ""},
-		{20, "stitched", "Black w/ orange", "No type", "Silicone", 8, 4, "16-24", 750.00, ""},
-		{21, "stitched", "Black w/ red", "No type", "Silicone", 8, 4, "16-24", 750.00, ""},
-		{22, "stitched", "Black w/ white", "No type", "Silicone", 8, 4, "16-24", 750.00, ""},
+		{1, "stitched", "Black w/ black", "No type", "Buffalo Calf", 10, 7, "16-24", 950, ""},
+		{2, "stitched", "Black w/ white", "No type", "Buffalo Calf", 10, 7, "16-24", 950, ""},
+		{3, "stitched", "Brown dark", "No type", "Buffalo Calf", 8, 4, "16-24", 950, ""},
+		{4, "stitched", "Brown light", "No type", "Buffalo Calf", 8, 4, "16-24", 950, ""},
+		{5, "stitched", "Black w/ black", "No type", "Chrono Genuine", 10, 7, "20-24", 1200, ""},
+		{6, "stitched", "Black w/ white", "No type", "Chrono Genuine", 10, 7, "20-24", 1200, ""},
+		{7, "stitched", "Brown w/ white", "No type", "Chrono Genuine", 8, 4, "20-24", 1200, ""},
+		{8, "stitched", "Black", "No type", "Genuine", 10, 7, "16-24", 650, ""},
+		{9, "stitched", "Brown dark", "No type", "Genuine", 10, 7, "16-24", 650, ""},
+		{10, "stitched", "Brown light", "No type", "Genuine", 18, 4, "16-24", 650, ""},
+		{11, "stitched", "Gray", "No type", "Genuine", 8, 4, "16-24", 650, ""},
+		{12, "stitched", "Red", "No type", "Genuine", 8, 4, "16-24", 650, ""},
+		{13, "stitched", "Black w/ black", "No type", "Cobra Genuine", 10, 7, "16-24", 550, ""},
+		{14, "stitched", "Blue light w/ blue", "No type", "Cobra Genuine", 8, 4, "16-24", 550, ""},
+		{15, "stitched", "Brown dark w/ brown", "No type", "Cobra Genuine", 10, 7, "16-24", 550, ""},
+		{16, "stitched", "Brown light w/ brown", "No type", "Cobra Genuine", 8, 4, "16-24", 550, ""},
+		{17, "stitched", "Gray w/ blue", "No type", "Cobra Genuine", 8, 4, "16-24", 550, ""},
+		{18, "stitched", "Red w/ red", "No type", "Cobra Genuine", 8, 4, "16-24", 550, ""},
+		{19, "stitched", "Black w/ blue", "No type", "Silicone", 8, 4, "16-24", 750, ""},
+		{20, "stitched", "Black w/ orange", "No type", "Silicone", 8, 4, "16-24", 750, ""},
+		{21, "stitched", "Black w/ red", "No type", "Silicone", 8, 4, "16-24", 750, ""},
+		{22, "stitched", "Black w/ white", "No type", "Silicone", 8, 4, "16-24", 750, ""},
 
-		{23, "no stitch", "Black", "Alpha", "No type", 10, 7, "16-24", 1500.00, ""},
-		{24, "no stitch", "Brown dark", "Alpha", "No type", 10, 7, "16-24", 1500.00, ""},
-		{25, "no stitch", "Brown light", "Alpha", "No type", 10, 7, "16-24", 1500.00, ""},
-		{26, "no stitch", "Red", "Alpha", "No type", 8, 4, "16-24", 1500.00, ""},
-		{27, "no stitch", "Black", "No type", "Brazilian", 10, 7, "16-24", 1200.00, ""},
-		{28, "no stitch", "Brown dark", "No type", "Brazilian", 8, 4, "16-24", 1200.00, ""},
-		{29, "no stitch", "Brown light", "No type", "Brazilian", 8, 4, "16-24", 1200.00, ""},
-		{30, "no stitch", "Black", "No type", "Buffalo Swiss", 10, 7, "16-24", 750.00, ""},
-		{31, "no stitch", "Blue", "No type", "Buffalo Swiss", 8, 4, "16-24", 750.00, ""},
-		{32, "no stitch", "Brown light", "No type", "Buffalo Swiss", 8, 4, "16-24", 750.00, ""},
-		{33, "no stitch", "Green", "No type", "Buffalo Swiss", 8, 4, "16-24", 750.00, ""},
-		{34, "no stitch", "White", "No type", "Buffalo Swiss", 10, 7, "16-24", 750.00, ""},
-		{35, "no stitch", "Black", "No type", "Genuine", 10, 7, "24", 850.00, ""},
-		{36, "no stitch", "Black", "No type", "Genuine Italy", 10, 7, "16-24", 650.00, ""},
-		{37, "no stitch", "Brown dark", "No type", "Genuine Italy", 8, 4, "16-24", 650.00, ""},
-		{38, "no stitch", "Brown light", "No type", "Genuine Italy", 8, 4, "16-24", 650.00, ""},
-		{39, "no stitch", "Black", "No type", "Silicone", 8, 4, "16-24", 750.00, ""},
-		{40, "no stitch", "Brown", "No type", "Silicone", 8, 4, "16-24", 750.00, ""},
-		{41, "no stitch", "Green", "No type", "Silicone", 8, 4, "16-24", 750.00, ""},
-		{42, "no stitch", "Orange", "No type", "Silicone", 8, 4, "16-24", 750.00, ""},
-		{43, "no stitch", "Red", "No type", "Silicone", 8, 4, "16-24", 750.00, ""},
-		{44, "no stitch", "Violet", "No type", "Silicone", 8, 4, "16-24", 750.00, ""},
-		{45, "no stitch", "White", "No type", "Silicone", 8, 4, "16-24", 750.00, ""},
-		{46, "no stitch", "Black", "No type", "Vintage Calf Genuine", 10, 7, "18-24", 1500.00, ""},
-		{47, "no stitch", "Brown dark", "No type", "Vintage Calf Genuine", 10, 7, "18-24", 1500.00, ""},
-		{48, "no stitch", "Brown light", "No type", "Vintage Calf Genuine", 8, 4, "18-24", 1500.00, ""},
-		{49, "no stitch", "Orange", "No type", "Vintage Calf Genuine", 8, 4, "18-24", 1500.00, ""},
-		{50, "no stitch", "Red light", "No type", "Vintage Calf Genuine", 8, 4, "18-24", 1500.00, ""},
-		{51, "no stitch", "Yellow", "No type", "Vintage Calf Genuine", 8, 4, "18-24", 1500.00, ""},
+		{23, "no stitch", "Black", "Alpha", "No type", 10, 7, "16-24", 1500, ""},
+		{24, "no stitch", "Brown dark", "Alpha", "No type", 10, 7, "16-24", 1500, ""},
+		{25, "no stitch", "Brown light", "Alpha", "No type", 10, 7, "16-24", 1500, ""},
+		{26, "no stitch", "Red", "Alpha", "No type", 8, 4, "16-24", 1500, ""},
+		{27, "no stitch", "Black", "No type", "Brazilian", 10, 7, "16-24", 1200, ""},
+		{28, "no stitch", "Brown dark", "No type", "Brazilian", 8, 4, "16-24", 1200, ""},
+		{29, "no stitch", "Brown light", "No type", "Brazilian", 8, 4, "16-24", 1200, ""},
+		{30, "no stitch", "Black", "No type", "Buffalo Swiss", 10, 7, "16-24", 750, ""},
+		{31, "no stitch", "Blue", "No type", "Buffalo Swiss", 8, 4, "16-24", 750, ""},
+		{32, "no stitch", "Brown light", "No type", "Buffalo Swiss", 8, 4, "16-24", 750, ""},
+		{33, "no stitch", "Green", "No type", "Buffalo Swiss", 8, 4, "16-24", 750, ""},
+		{34, "no stitch", "White", "No type", "Buffalo Swiss", 10, 7, "16-24", 750, ""},
+		{35, "no stitch", "Black", "No type", "Genuine", 10, 7, "24", 850, ""},
+		{36, "no stitch", "Black", "No type", "Genuine Italy", 10, 7, "16-24", 650, ""},
+		{37, "no stitch", "Brown dark", "No type", "Genuine Italy", 8, 4, "16-24", 650, ""},
+		{38, "no stitch", "Brown light", "No type", "Genuine Italy", 8, 4, "16-24", 650, ""},
+		{39, "no stitch", "Black", "No type", "Silicone", 8, 4, "16-24", 750, ""},
+		{40, "no stitch", "Brown", "No type", "Silicone", 8, 4, "16-24", 750, ""},
+		{41, "no stitch", "Green", "No type", "Silicone", 8, 4, "16-24", 750, ""},
+		{42, "no stitch", "Orange", "No type", "Silicone", 8, 4, "16-24", 750, ""},
+		{43, "no stitch", "Red", "No type", "Silicone", 8, 4, "16-24", 750, ""},
+		{44, "no stitch", "Violet", "No type", "Silicone", 8, 4, "16-24", 750, ""},
+		{45, "no stitch", "White", "No type", "Silicone", 8, 4, "16-24", 750, ""},
+		{46, "no stitch", "Black", "No type", "Vintage Calf Genuine", 10, 7, "18-24", 1500, ""},
+		{47, "no stitch", "Brown dark", "No type", "Vintage Calf Genuine", 10, 7, "18-24", 1500, ""},
+		{48, "no stitch", "Brown light", "No type", "Vintage Calf Genuine", 8, 4, "18-24", 1500, ""},
+		{49, "no stitch", "Orange", "No type", "Vintage Calf Genuine", 8, 4, "18-24", 1500, ""},
+		{50, "no stitch", "Red light", "No type", "Vintage Calf Genuine", 8, 4, "18-24", 1500, ""},
+		{51, "no stitch", "Yellow", "No type", "Vintage Calf Genuine", 8, 4, "18-24", 1500, ""},
 
-		{52, "casio", "Black", "Casio", "No type", 10, 7, "Fixed", 750.00, "1 hole"},
-		{53, "casio", "Blue dark", "Casio", "No type", 8, 4, "Fixed", 750.00, "1 hole"},
-		{54, "casio", "Blue light", "Casio", "No type", 8, 4, "Fixed", 750.00, "1 hole"},
-		{55, "casio", "Orange", "Casio", "No type", 8, 4, "Fixed", 750.00, "1 hole"},
-		{56, "casio", "Pink dark", "Casio", "No type", 8, 4, "Fixed", 750.00, "1 hole"},
-		{57, "casio", "Pink light", "Casio", "No type", 8, 4, "Fixed", 750.00, "1 hole"},
-		{58, "casio", "White", "Casio", "No type", 10, 7, "Fixed", 750.00, "1 hole"},
+		{52, "casio", "Black", "Casio", "No type", 10, 7, "Fixed", 750, "1 hole"},
+		{53, "casio", "Blue dark", "Casio", "No type", 8, 4, "Fixed", 750, "1 hole"},
+		{54, "casio", "Blue light", "Casio", "No type", 8, 4, "Fixed", 750, "1 hole"},
+		{55, "casio", "Orange", "Casio", "No type", 8, 4, "Fixed", 750, "1 hole"},
+		{56, "casio", "Pink dark", "Casio", "No type", 8, 4, "Fixed", 750, "1 hole"},
+		{57, "casio", "Pink light", "Casio", "No type", 8, 4, "Fixed", 750, "1 hole"},
+		{58, "casio", "White", "Casio", "No type", 10, 7, "Fixed", 750, "1 hole"},
 
-		{59, "casio", "Black", "Casio", "No type", 10, 7, "Fixed", 950.00, "2 hole"},
-		{60, "casio", "Blue dark", "Casio", "No type", 8, 4, "Fixed", 950.00, "2 hole"},
-		{61, "casio", "Gray", "Casio", "No type", 8, 4, "Fixed", 950.00, "2 hole"},
-		{62, "casio", "Red", "Casio", "No type", 8, 4, "Fixed", 950.00, "2 hole"},
-		{63, "casio", "White", "Casio", "No type", 10, 7, "Fixed", 950.00, "2 hole"},
+		{59, "casio", "Black", "Casio", "No type", 10, 7, "Fixed", 950, "2 hole"},
+		{60, "casio", "Blue dark", "Casio", "No type", 8, 4, "Fixed", 950, "2 hole"},
+		{61, "casio", "Gray", "Casio", "No type", 8, 4, "Fixed", 950, "2 hole"},
+		{62, "casio", "Red", "Casio", "No type", 8, 4, "Fixed", 950, "2 hole"},
+		{63, "casio", "White", "Casio", "No type", 10, 7, "Fixed", 950, "2 hole"},
 
-		{64, "casio w/ cover", "Black", "Casio", "No type", 8, 4, "Small", 1200.00, "1 hole"},
-		{65, "casio w/ cover", "Blue dark", "Casio", "No type", 8, 4, "Small", 1200.00, "1 hole"},
-		{66, "casio w/ cover", "Red", "Casio", "No type", 8, 4, "Small", 1200.00, "1 hole"},
-		{67, "casio w/ cover", "Violet", "Casio", "No type", 8, 4, "Small", 1200.00, "1 hole"},
-		{68, "casio w/ cover", "White", "Casio", "No type", 8, 4, "Small", 1200.00, "1 hole"},
+		{64, "casio w/ cover", "Black", "Casio", "No type", 8, 4, "Small", 1200, "1 hole"},
+		{65, "casio w/ cover", "Blue dark", "Casio", "No type", 8, 4, "Small", 1200, "1 hole"},
+		{66, "casio w/ cover", "Red", "Casio", "No type", 8, 4, "Small", 1200, "1 hole"},
+		{67, "casio w/ cover", "Violet", "Casio", "No type", 8, 4, "Small", 1200, "1 hole"},
+		{68, "casio w/ cover", "White", "Casio", "No type", 8, 4, "Small", 1200, "1 hole"},
 
-		{69, "casio w/ cover", "Black", "Casio", "No type", 10, 7, "Large", 1500.00, "2 hole"},
-		{70, "casio w/ cover", "Blue dark", "Casio", "No type", 8, 4, "Large", 1500.00, "2 hole"},
-		{71, "casio w/ cover", "Orange", "Casio", "No type", 8, 4, "Large", 1500.00, "2 hole"},
-		{72, "casio w/ cover", "Red", "Casio", "No type", 8, 4, "Large", 1500.00, "2 hole"},
-		{73, "casio w/ cover", "White", "Casio", "No type", 10, 7, "Large", 1500.00, "2 hole"}};
+		{69, "casio w/ cover", "Black", "Casio", "No type", 10, 7, "Large", 1500, "2 hole"},
+		{70, "casio w/ cover", "Blue dark", "Casio", "No type", 8, 4, "Large", 1500, "2 hole"},
+		{71, "casio w/ cover", "Orange", "Casio", "No type", 8, 4, "Large", 1500, "2 hole"},
+		{72, "casio w/ cover", "Red", "Casio", "No type", 8, 4, "Large", 1500, "2 hole"},
+		{73, "casio w/ cover", "White", "Casio", "No type", 10, 7, "Large", 1500, "2 hole"}};
 
-	int DisplayStitched(vector<StrapItems> arr, string categ)
-	{
+	int DisplayStitched(vector<StrapItems> arr, string categ){
+		cout << fixed << setprecision(2);
 		cout << "\n " << categ << " \n";
 		cout << right << setw(10) << " ";
 		cout << left << setw(5) << "ID"
@@ -155,8 +155,8 @@ public:
 		return 0;
 	}
 
-	int DisplayNoStitched(vector<StrapItems> arr, string categ)
-	{
+	int DisplayNoStitched(vector<StrapItems> arr, string categ){
+		cout << fixed << setprecision(2);
 		cout << "\n " << categ << " \n";
 		cout << right << setw(10) << " ";
 		cout << left << setw(5) << "ID"
@@ -184,8 +184,8 @@ public:
 		return 0;
 	}
 
-	int DisplayCasioAdult(vector<StrapItems> arr, string categ)
-	{
+	int DisplayCasioAdult(vector<StrapItems> arr, string categ){
+		cout << fixed << setprecision(2);
 		cout << "\n " << categ << " \n";
 		cout << right << setw(10) << " ";
 		cout << left << setw(5) << "ID"
@@ -233,8 +233,8 @@ public:
 		return 0;
 	}
 
-	int DisplayCasioKids(vector<StrapItems> arr, string categ)
-	{
+	int DisplayCasioKids(vector<StrapItems> arr, string categ){
+		cout << fixed << setprecision(2);
 		cout << "\n " << categ << " \n";
 		cout << right << setw(10) << " ";
 		cout << left << setw(5) << "ID"
